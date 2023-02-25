d13d7994f907ed4ec5b83a8fef060580c7725f22
JJ Andrew <jjandrew003@gmail.com>
2023-02-25 14:31:42 +0000

    Checklist for rooms

--- submission/urls.py
+++ submission/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_name, name='get_name'),
+    path('', views.input_building, name='get_name'),
 ]
--- submission/views.py
+++ submission/views.py
@@ -1,23 +1,23 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from .forms import NameForm
+from .forms import RoomInputForm
 
 
-def get_name(request):
+def input_building(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
+        form = RoomInputForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            return HttpResponseRedirect('/')
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = NameForm()
+        form = RoomInputForm()
 
     return render(request, 'submission/submission.html', {'form': form})
