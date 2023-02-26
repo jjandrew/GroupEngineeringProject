6104ced1b193c72879abc2e8c92541b5adc8090f
JJ Andrew <jjandrew003@gmail.com>
2023-02-26 12:52:07 +0000

    Added user model to DB

--- loginApp/models.py
+++ loginApp/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+
+class User(models.Model):
+    username = models.CharField(max_length=150)
+    points = models.IntegerField()
+
+    def __str__(self):
+        return self.username
