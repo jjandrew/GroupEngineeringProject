65fa4e78f997210f40d85b306b929564497e3966
JJ Andrew <jjandrew003@gmail.com>
2023-02-26 16:45:44 +0000

    Added more tests for loginApp

--- loginApp/tests.py
+++ loginApp/tests.py
@@ -42,6 +42,7 @@ class UserModelTestCase(TestCase):
             self.fail("Can't create username with 150 chars")
 
     def test_user_points_cant_be_neg(self):
+        """Make sure user can't be created with negative points"""
         try:
             user = User(username="Test user", points=-1)
             user.save()
@@ -50,6 +51,7 @@ class UserModelTestCase(TestCase):
             pass
 
     def test_username_must_be_unique(self):
+        """Test username must be unique"""
         try:
             user = User(username="testuser", points=0)
             user.save()
@@ -58,3 +60,14 @@ class UserModelTestCase(TestCase):
             self.fail("Created two users with the same username")
         except:
             pass
+
+    def test_points_doesnt_have_to_be_unique(self):
+        """Test username must be unique"""
+        try:
+            user = User(username="testuser", points=0)
+            user.save()
+            user1 = User(username="testuser1", points=0)
+            user1.save()
+            pass
+        except:
+            self.fail("Can't create two user's with same points")
