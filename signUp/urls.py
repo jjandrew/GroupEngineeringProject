834153f45f22b0cbc496dcc419dd10613b192170
JJ Andrew <jjandrew003@gmail.com>
2023-02-26 12:33:24 +0000

    Fixed bug with signUp url and checks user is authenticated when submitting room

--- signUp/urls.py
+++ signUp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserSignUpView
 
 urlpatterns = [
-    path("signup/", UserSignUpView.as_view(), name="signUp"),
+    path("", UserSignUpView.as_view(), name="signUp"),
 ]
