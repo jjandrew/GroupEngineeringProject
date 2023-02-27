4ea5c507ded1aae5a00ad9716f05b4b3a2342b27
JJ Andrew <jjandrew003@gmail.com>
2023-02-27 16:14:24 +0000

    Added other attributes to user

--- loginApp/models.py
+++ loginApp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+import uuid
 
 
 class User(models.Model):
     username = models.CharField(max_length=50, unique=True)
-    email = models.CharField(max_length=50, unique=True)
+    email = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     points = models.PositiveIntegerField()
