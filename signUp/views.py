5fbb1cca14ac8850e0d90a3dd7c1711ecc6c25f7
louislusso <louislusso@hotmail.com>
2023-02-27 15:24:32 +0000

    Added firstname and lastname tabs when signing up

--- signUp/views.py
+++ signUp/views.py
@@ -4,16 +4,16 @@ from .forms import SignUpForm
 
 
 def signup(request):
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = form.save()
-            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-            return redirect('home')
-        context = {
-            'form': form
-        }
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = form.save()
+        login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+        return redirect('login')
+    context = {
+        'form': form
+    }
 
-        return render(request, 'registration/signup.html', context)
+    return render(request, 'registration/signup.html', context)
