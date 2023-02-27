0be836ee73f64a6fc470855bd0900c1f314d3d57
JJ Andrew <jjandrew003@gmail.com>
2023-02-27 14:43:59 +0000

    Fixed error where user can have negative points

--- loginApp/models.py
+++ loginApp/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator
 
 class User(models.Model):
     username = models.CharField(max_length=50)
-    points = models.IntegerField(validators=[MinValueValidator(0)])
+    points = models.PositiveIntegerField()
 
     def __str__(self):
         return self.username
