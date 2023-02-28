aea0d792ffb8d0ea5d953527a80245346f48cb10
JJ Andrew <jjandrew003@gmail.com>
2023-02-28 20:57:29 +0000

    Tried to fix migrations issue

--- loginApp/urls.py
+++ loginApp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView 
+from django.views.generic.base import TemplateView
 from . import views
 urlpatterns = [
     # Path for the admin URL
