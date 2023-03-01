ca5d9bc4167205c03dfb661a0f6c91c419dc5b04
JJ Andrew <jjandrew003@gmail.com>
2023-03-01 12:41:22 +0000

    Leaderboard now done off custom user and removed add to leaderboard button

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import leaderboard, add_to_leaderboard
+from .views import leaderboard
 
 urlpatterns = [
     path('', leaderboard, name='leaderboard'),
-    path('add-to-leaderboard/', add_to_leaderboard, name='add_to_leaderboard'),
 ]
--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render, redirect
 from loginApp.models import User
 from django.contrib.auth.decorators import login_required
+from accounts.models import CustomUser
+
 
 @login_required
 def leaderboard(request):
-    leaderboard = User.objects.order_by('-points')
+    leaderboard = CustomUser.objects.order_by('-points')
     return render(request, 'leaderboard/leaderboard.html', {'leaderboard': leaderboard})
-
-@login_required
-def add_to_leaderboard(request):
-    if request.method == 'POST':
-        player_name = request.POST.get('player_name')
-        score = request.POST.get('score')
-        leaderboard_entry = User(username=player_name, points=score,
-                                 email='test@test.com', first_name="testf", last_name="testl")
-        leaderboard_entry.save()
-        return redirect('leaderboard')
-    else:
-        return render(request, 'leaderboard/add_to_leaderboard.html')
