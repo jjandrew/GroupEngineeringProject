d8b8db3c7c2e8c1d2438cb92fa7d915a175b3f14
jjandrew <jja211@exeter.ac.uk>
2023-03-14 13:07:48 +0000

    Added more comments and removed sent emails and media from git

--- technical-documents/source-code/leaderboard/urls.py
+++ technical-documents/source-code/leaderboard/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import leaderboard, workingLeaderboard
 
+""" Defines and names the url that displays the leaderboard.  """
 urlpatterns = [
-    # Defines and names the url that displays the leaderboard.
     path('', workingLeaderboard, name='leaderboard'),
     path('UI', leaderboard, name='workingLweaderboard'),
 ]
