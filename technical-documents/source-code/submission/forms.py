eef3f2d68363f2def6b5e229fb619ed0acd11350
jjandrew <jja211@exeter.ac.uk>
2023-03-18 14:42:04 +0000

    Removed unecessary parts to submission

--- technical-documents/source-code/submission/forms.py
+++ technical-documents/source-code/submission/forms.py
@@ -12,5 +12,5 @@ class ImageForm(forms.ModelForm):
         validators to be created.
         """
         model = ImageSubmission
-        fields = ('building', 'room', 'number_of_lights', 'number_lights_on', 'number_of_windows',
-                  'number_windows_open', 'number_plugs', 'number_plugs_on', 'litter_items', 'image')
+        fields = ('building', 'room', 'lights_status',
+                  'windows_status', 'litter_items', 'image')
