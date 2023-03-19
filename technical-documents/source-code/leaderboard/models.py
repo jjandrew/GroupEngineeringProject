9bc357de952e87cef3a5c379b4b34d75ce585fed
jjandrew <jja211@exeter.ac.uk>
2023-03-19 18:23:00 +0000

    Collected energy usage per building

--- technical-documents/source-code/leaderboard/models.py
+++ technical-documents/source-code/leaderboard/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from submission.models import building_choices
+from datetime import datetime
+
+
+class BuildingModel(models.Model):
+    """A model for a room in order to calculate environmental statistics"""
+    name = models.CharField(choices=building_choices, max_length=48)
+    stats_since = models.DateField(
+        default=datetime.today().strftime('%Y-%m-%d'))
+    co2 = models.PositiveIntegerField(default=0)
