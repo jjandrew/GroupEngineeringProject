bbd44c12cd5328e41135a82c515f951e6fa402c9
jjandrew <jja211@exeter.ac.uk>
2023-03-19 21:57:48 +0000

    Added one point per day building hasn't been checked

--- technical-documents/source-code/leaderboard/models.py
+++ technical-documents/source-code/leaderboard/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from submission.models import building_choices
+from datetime import datetime
+
+
+class BuildingModel(models.Model):
+    """A model for a room in order to calculate environmental statistics"""
+    name = models.CharField(choices=building_choices, max_length=48)
+    stats_since = models.DateField(
+        default=datetime.today().strftime('%Y-%m-%d'))
+    co2 = models.PositiveIntegerField(default=0)
+    last_done = models.DateTimeField(default='2023-01-01 00:00:00')
--- technical-documents/source-code/submission/crowd_source.py
+++ technical-documents/source-code/submission/crowd_source.py
@@ -1,9 +1,11 @@
 from submission.models import RoomModel, ImageSubmission
 from datetime import datetime
+from leaderboard.models import BuildingModel
 
 
 def input_stats(submission: ImageSubmission):
     """Inputs the stats for an image submission into """
+    # Check if a room exists
     room = None
     if RoomModel.objects.filter(building=submission.building, name=submission.room.lower()).exists():
         room = RoomModel.objects.get(
@@ -12,6 +14,13 @@ def input_stats(submission: ImageSubmission):
         room = RoomModel(name=submission.room.lower(),
                          building=submission.building)
         room.save()
+
+    # Check if building exists and if not create a new building object
+    building = None
+    if not BuildingModel.objects.filter(name=submission.building).exists():
+        building = BuildingModel(name=submission.building)
+        building.save()
+
     add_stats(submission, room)
 
 
