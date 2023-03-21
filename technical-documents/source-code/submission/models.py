9294c3e4752985e07780d38079fd47d3556372f5
jjandrew <jja211@exeter.ac.uk>
2023-03-21 11:14:39 +0000

    Fixed merging errors and added message to submission

--- technical-documents/source-code/submission/models.py
+++ technical-documents/source-code/submission/models.py
@@ -92,4 +92,4 @@ class RoomModel(models.Model):
     number_windows_open = models.PositiveIntegerField(default=0)
     litter_items = models.PositiveIntegerField(default=0)
     number_submissions = models.PositiveIntegerField(default=0)
-    last_done = models.DateTimeField(default='2023-01-01 00:00:00')
+    last_done = models.DateTimeField(default='2023-01-01 00:00:00 +0000')
