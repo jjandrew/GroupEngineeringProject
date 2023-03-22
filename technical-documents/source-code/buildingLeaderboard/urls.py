d96deb14567845ac53a5e0729b593aefb9abd2ff
jjandrew <jja211@exeter.ac.uk>
2023-03-22 19:04:20 +0000

    Building module leaderboard pylint

--- technical-documents/source-code/buildingLeaderboard/urls.py
+++ technical-documents/source-code/buildingLeaderboard/urls.py
@@ -1,7 +1,7 @@
+""" Defines and names the url that displays the leaderboard.  """
 from django.urls import path
 from .views import buildingLeaderboard
 
-""" Defines and names the url that displays the leaderboard.  """
 urlpatterns = [
     # Defines and names the url that displays the leaderboard.
     path('', buildingLeaderboard, name='buildingLeaderboard'),
