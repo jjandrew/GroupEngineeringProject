ca1b8f4fdabe3cfc606445bdb20190def4e3e98c
BenjaminTheron <86923082+BenjaminTheron@users.noreply.github.com>
2023-03-22 18:41:15 +0000

    PEP8 Commenting and Formatting for the GK homepage

--- technical-documents/source-code/gkHomepage/crowd_source.py
+++ technical-documents/source-code/gkHomepage/crowd_source.py
@@ -1,9 +1,20 @@
-from submission.models import RoomModel, ImageSubmission, building_choices
+""" Defines how data for the CO2 collection will be crowdsourced. """
 from datetime import datetime
+from submission.models import RoomModel, ImageSubmission, building_choices
 from leaderboard.models import BuildingModel
 
 
-def get_building_name(top_sub):
+def get_building_name(top_sub) -> str:
+    """ Gets the name of the building.
+
+    Args:
+        top_sub: The top building submission, which is having it's
+            name collected.
+
+    Returns:
+        str: building_name: The name of the building as a string for ease of
+            processing.
+    """
     # Translate Constant building name to formatted string
     building_name = None
     for choice in building_choices:
@@ -11,16 +22,21 @@ def get_building_name(top_sub):
             building_name = choice[1]
             break
     if building_name == None:
-        # TODO remove this
         print("Collosal error")
     return building_name
 
 
-def input_stats(submission: ImageSubmission):
-    """Inputs the stats for an image submission into """
+def input_stats(submission: ImageSubmission) -> None:
+    """ Inputs the stats for an image submission into.
+
+    Args:
+        submission: (ImageSubmission): The ImageSubmission model which is
+            having it's input stats added to it.
+    """
     # Check if a room exists
     room = None
-    if RoomModel.objects.filter(building=submission.building, name=submission.room.lower()).exists():
+    if RoomModel.objects.filter(building=submission.building,
+                                name=submission.room.lower()).exists():
         room = RoomModel.objects.get(
             name=submission.room.lower(), building=submission.building)
     else:
@@ -38,8 +54,15 @@ def input_stats(submission: ImageSubmission):
     add_stats(submission, room)
 
 
-def add_stats(submission: ImageSubmission, room: RoomModel):
-    """Add the stats from submission to room model"""
+def add_stats(submission: ImageSubmission, room: RoomModel) -> None:
+    """ Add the stats from submission to room model.
+
+    Args:
+        submission: (ImageSubmission): The ImageSubmission object representing
+            the submission from which the stats are being extracted.
+        room: (RoomModel): The Room Model object of the room which is having
+            stats added to it.
+    """
     if submission.lights_status == "ON":
         room.number_lights_on += 1
     if submission.windows_status == "OPEN":
