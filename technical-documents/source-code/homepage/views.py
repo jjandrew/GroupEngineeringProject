16ff3ac9316212e540f82f6b85a70887bc9a9b04
jjandrew <jja211@exeter.ac.uk>
2023-03-22 16:34:53 +0000

    Fixed homepage bug

--- technical-documents/source-code/homepage/views.py
+++ technical-documents/source-code/homepage/views.py
@@ -6,36 +6,35 @@ from accounts.models import CustomUser
 from datetime import date
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 def get_daily_task(user):
 
-
     # Check if the user has a daily task assigned for today
     if user.last_daily_task != date.today():
         # Generate a new daily task and assign it to the user
 
-        user.daily_task = random.randint(0, 43)#44 options
+        user.daily_task = random.randint(0, 43)  # 44 options
         user.last_daily_task = date.today()
         user.save()
 
 
+@login_required
 def index(request):
     user = request.user
-    #get_daily_task(user)
+    # get_daily_task(user)
 
-
-
-    building_choices = ['Alexander','Amory', 'Business School Building:One','Byrne House', 'Clayden',
-                        'Clydesdale House','Cornwall House','Cornwall House Swimming Pool','Devonshire House',
-                        'Estate Service Centre','Family Centre (Owlets)','Forum and Library','Geoffrey Pope',
-                        'Great Hall and University Reception','Harrison','Hatherly','Henry Wellcome Building for Biocatalysis',
-                        'Innovation Centre Phase 1 and 2','Institute of Arab and Islamic Studies','INTO International Study Centre',
-                        'Kay House Duryard','Knightley','Lafrowda House','Laver','Lazenby','Mary Harris Memorial Chapel','Newman',
-                        'Northcote House','Northcott Theatre','Old Library','Peter Chalk Centre','Physics',"Queen's",'Redcot','Reed Hall',
-                        'Reed Mews Wellbeing Centre','Roborough','Russell Seal Fitness Centre','Sir Christopher Ondaatje Devon Cricket Centre',
-                        'Sir Henry Wellcome Building for Mood Disorders Research','Streatham Court','Streatham Farm','Washington Singer','Xfi']
+    building_choices = ['Alexander', 'Amory', 'Business School Building:One', 'Byrne House', 'Clayden',
+                        'Clydesdale House', 'Cornwall House', 'Cornwall House Swimming Pool', 'Devonshire House',
+                        'Estate Service Centre', 'Family Centre (Owlets)', 'Forum and Library', 'Geoffrey Pope',
+                        'Great Hall and University Reception', 'Harrison', 'Hatherly', 'Henry Wellcome Building for Biocatalysis',
+                        'Innovation Centre Phase 1 and 2', 'Institute of Arab and Islamic Studies', 'INTO International Study Centre',
+                        'Kay House Duryard', 'Knightley', 'Lafrowda House', 'Laver', 'Lazenby', 'Mary Harris Memorial Chapel', 'Newman',
+                        'Northcote House', 'Northcott Theatre', 'Old Library', 'Peter Chalk Centre', 'Physics', "Queen's", 'Redcot', 'Reed Hall',
+                        'Reed Mews Wellbeing Centre', 'Roborough', 'Russell Seal Fitness Centre', 'Sir Christopher Ondaatje Devon Cricket Centre',
+                        'Sir Henry Wellcome Building for Mood Disorders Research', 'Streatham Court', 'Streatham Farm', 'Washington Singer', 'Xfi']
 
     taskNumber = user.daily_task
     task = building_choices[taskNumber]
@@ -46,7 +45,6 @@ def index(request):
     points = request.user.points
     streak = request.user.streak
 
-    args = {'username': username, 'email': email, 'points': points, 'streak': streak, 'task': task}
+    args = {'username': username, 'email': email,
+            'points': points, 'streak': streak, 'task': task}
     return render(request, "homepage/homepage.html", args)
-
-
